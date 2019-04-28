3c1fa41faaf3a889159f8d7e9c24f4d03d501aed
szp <1103036472@qq.com>
2019-04-28 22:04:10 +0800

    增加格点类 优化代码

--- function.py
+++ function.py
@@ -1,16 +1,26 @@
-import Node
 import Settings
 import math
 
 
-def find_distance(node1, node2):
+def get_grid_sensor_distance(grid_point, node):
     """
-    求两节点间的距离
+    求格点与某个传感器间的距离
+    :param grid_point:
+    :param node:
+    :return:
+    """
+    distance = math.sqrt((grid_point.x - node.x) ** 2 + (grid_point.y - node.y) ** 2)
+    return distance
+
+
+def get_sensor_distance(node1, node2):
+    """
+    求两传感器间的距离
     :param node1:
     :param node2:
     :return: 距离（保留两位小数）
     """
-    distance = math.sqrt((node2.x - node1.x) * (node2.x - node1.x) + (node2.y - node1.y) * (node2.y - node1.y))
+    distance = math.sqrt((node2.x - node1.x) ** 2 + (node2.y - node1.y) ** 2)
     # 发送广播数据包请求距离
     node1.send_datapack(Settings.broadcast_datapack, distance)
     node2.receive_databack(Settings.broadcast_datapack)
@@ -27,7 +37,7 @@ def judge_close(node1, node2):
     :param node2:
     :return:
     """
-    distance = find_distance(node1, node2)
+    distance = get_sensor_distance(node1, node2)
     scope = Settings.a * node1.PERCEIVED_RADIUS
     if distance <= scope:
         return True
@@ -61,3 +71,65 @@ def get_active_nodes(node, sensor):
         if node.is_asleep:
             active_node_list.remove(node)
     return active_node_list
+
+
+def judge_in_two_circles(grid_point, node1, node2):
+    """
+    判断某个格点是否在两个传感器感知范围
+    :param grid_point:
+    :param node1:
+    :param node2:
+    :return:
+    """
+    distance1 = get_grid_sensor_distance(grid_point, node1)
+    distance2 = get_grid_sensor_distance(grid_point, node2)
+    if distance1 < node1.PERCEIVED_RADIUS and distance2 < node2.PERCEIVED_RADIUS:
+        return True
+    else:
+        return False
+
+
+def get_in_grid_points_num(grid_points, node1, node2):
+    """
+    获取两个传感器节点内的所有格点
+    :param grid_points:
+    :param node1:
+    :param node2:
+    :return:
+    """
+    num = 0
+    for grid_point in grid_points:
+        if judge_in_two_circles(grid_point, node1, node2):
+            num += 1
+    return num
+
+
+def judge_on_two_circles(grid_point, node1, node2):
+    """
+    判断某个格点是否在两个传感器感知半径上
+    :param grid_point:
+    :param node1:
+    :param node2:
+    :return:
+    """
+    distance1 = get_grid_sensor_distance(grid_point, node1)
+    distance2 = get_grid_sensor_distance(grid_point, node2)
+    if distance1 == node1.PERCEIVED_RADIUS or distance2 == node2.PERCEIVED_RADIUS:
+        return True
+    else:
+        return False
+
+
+def get_on_grid_points_num(grid_points, node1, node2):
+    """
+    获取两个传感器节点上的所有格点
+    :param grid_points:
+    :param node1:
+    :param node2:
+    :return:
+    """
+    num = 0
+    for grid_point in grid_points:
+        if judge_on_two_circles(grid_point, node1, node2):
+            num += 1
+    return num
